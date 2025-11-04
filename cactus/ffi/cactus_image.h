413e49239eb1ebb2bcb6fc1f4005ec666cd6866d
justinl66 <justinfinity7@gmail.com>
2025-11-03 23:11:54 -0800

    Combine image header files into singular cactus_image.h
    Signed-off-by: justinl66 <justinfinity7@gmail.com>

---
+++ cactus/ffi/cactus_image.h
@@ -0,0 +1,9 @@
+#ifndef CACTUS_STB_IMAGE_IMPL_H
+#define CACTUS_STB_IMAGE_IMPL_H
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+
+#include "stb_image_combined.h"
+
+#endif
